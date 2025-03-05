44f39845442c92de8157b9fc087c8d785da23751
Serge Babayan <babayan.serge@gmail.com>
2025-03-05 21:43:26 +0300

    Add atomic flag is_success

--- main.cpp
+++ main.cpp
@@ -3,11 +3,15 @@
 #include <limits>
 #include <vector>
 #include <thread>
+// #include <mutex>
+#include <atomic>
 
 #include "CRC32.hpp"
 #include "IO.hpp"
 
-static bool is_success = false;
+// static bool is_success = false;
+static std::atomic<bool> is_success(false);
+std::mutex mtx;
 
 struct Range {
     Range(uint32_t start, uint32_t finish) : m_start_number(start), m_finish_number(finish) {}
@@ -51,23 +55,23 @@ void replaceLastFourBytes(std::vector<char> &data, uint32_t value) {
  * @return новый вектор
  */
 void hack(const std::vector<char> &original,
-                    const std::string &injection,
-                    const char* out_path,
-                    const uint32_t start,
-                    const uint32_t finish) {
+    const std::string &injection,
+    const char* out_path,
+    const uint32_t start,
+    const uint32_t finish) {
   const uint32_t originalCrc32 = crc32(original.data(), original.size());
   std::vector<char> result(original.size() + injection.size() + 4);
   auto it = std::copy(original.begin(), original.end(), result.begin());
   std::copy(injection.begin(), injection.end(), it);
 
   /*
-   * Внимание: код ниже крайне не оптимален.
-   * В качестве доп. задания устраните избыточные вычисления
-   */
+  * Внимание: код ниже крайне не оптимален.
+  * В качестве доп. задания устраните избыточные вычисления
+  */
 
   for (size_t i = start; i < finish; ++i) {
-    if (is_success) {
-        return;
+    if (is_success.load() == true) {
+      return;
     }
     // Заменяем последние четыре байта на значение i
     replaceLastFourBytes(result, uint32_t(i));
@@ -75,55 +79,55 @@ void hack(const std::vector<char> &original,
     auto currentCrc32 = crc32(result.data(), result.size());
 
     if (currentCrc32 == originalCrc32) {
-        writeToFile(out_path, result);
-        is_success = true;
-        std::cout << "Success\n";
-        return;
+      writeToFile(out_path, result);
+      is_success.store(true);
+      std::cout << "Success\n";
+      return;
     }
     // Отображаем прогресс
     if (i % 1000 == 0) {
+      std::unique_lock<std::mutex> ulock(mtx);
       std::cout << "progress: "
-                << static_cast<double>(i) / static_cast<double>(finish)
-                << std::endl;
+      << static_cast<double>(i) / static_cast<double>(finish)
+      << std::endl;
     }
   }
-  throw std::logic_error("Can't hack");
+  // throw std::logic_error("Can't hack");
 }
 
-int main(int argc, char **argv) {
-    int threads_amount = 1;
-    if (argc != 4) {
-      std::cerr << "Call with three args: " << argv[0]
-                << " <input file> <output file> <threads amount 1...100>\n";
-      return 1;
-    }
-    if (atoi(argv[3]) >= 1 && atoi(argv[3]) <= 100) {
-      threads_amount = atoi(argv[3]);
-    } else {
-      std::cerr << "The third argument must be a number 1...100\n";
-      return 1;
-    }
+int main(int argc, char** argv) {
+  int threads_amount = 1;
+  if (argc != 4) {
+    std::cerr << "Call with three args: " << argv[0]
+              << " <input file> <output file> <threads amount 1...100>\n";
+    return 1;
+  }
+  if (atoi(argv[3]) >= 1 && atoi(argv[3]) <= 100) {
+    threads_amount = atoi(argv[3]);
+  } else {
+    std::cerr << "The third argument must be a number 1...100\n";
+    return 1;
+  }
 
-    const std::string extra = "He-he-he";
-    const size_t maxVal = std::numeric_limits<uint32_t>::max();
-    std::vector<Range> ranges = get_ranges(threads_amount, maxVal);
-    std::vector<std::thread> threads;
-  
-    const auto start_time = std::chrono::steady_clock::now();
+  const std::string extra = "He-he-he";
+  const size_t maxVal = std::numeric_limits<uint32_t>::max();
+  std::vector<Range> ranges = get_ranges(threads_amount, maxVal);
+  std::vector<std::thread> threads;
+
+  const auto start_time = std::chrono::steady_clock::now();
 
   try {
     const std::vector<char> data = readFromFile(argv[1]);
 
-    for (Range rng : ranges) {
-        threads.emplace_back(hack, 
-                                ref(data), 
-                                ref(extra), 
-                                argv[2], 
-                                rng.m_start_number, 
-                                rng.m_finish_number);
+    for (Range rng : ranges) {threads.emplace_back(hack, 
+                              ref(data), 
+                              ref(extra), 
+                              argv[2], 
+                              rng.m_start_number, 
+                              rng.m_finish_number);
     }
     for (std::thread& t : threads) {
-        t.join();
+      t.join();
     }
 
   } catch (std::exception &ex) {
@@ -131,7 +135,7 @@ int main(int argc, char **argv) {
     return 2;
   }
 
-  const std::chrono::duration<double> dur = std::chrono::steady_clock::now() - start_time;
-  std::cout << "Duration: " << dur.count() << " sec" << std::endl;
-  return 0;
+const std::chrono::duration<double> dur = std::chrono::steady_clock::now() - start_time;
+std::cout << "Duration: " << dur.count() << " sec" << std::endl;
+return 0;
 }
